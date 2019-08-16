6d78d960d1a9607209d3c5fde4d35fab4f1417d8
JustinCCampbell3 <38171122+JustinCCampbell3@users.noreply.github.com>
2019-08-16 13:57:17 -0700

    .

--- script.js
+++ script.js
@@ -9,15 +9,41 @@ function closeSidebar() {
 
 //opens sign in form
 function signInUser() {
-    document.getElementById("id01").style.display = "block"; 
+    document.getElementById("id01").style.display = "block";
 }
 
-//closes sign in form 
+//closes sign in form
 function closeModal() {
-    document.getElementById("id01").style.display = "none"; 
+    document.getElementById("id01").style.display = "none";
 }
 
 function welcomeUser() {
-    var username = document.getElementById("uname").value; 
-    alert("Welcome back " + username + "!"); 
+    var username = document.getElementById("uname").value;
+    alert("Welcome back " + username + "!");
+}
+
+// Get the modal
+var modal = document.getElementById("descriptionModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
