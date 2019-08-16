b15ee54971c66709a521bab84177f469dfafae4c
JustinCCampbell3 <38171122+JustinCCampbell3@users.noreply.github.com>
2019-08-16 14:11:24 -0700

    .

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ function welcomeUser() {
 }
 
 // Get the modal
-var modal = document.getElementById("descriptionModal");
+var modal = document.getElementById("myModal");
 
 // Get the button that opens the modal
 var btn = document.getElementById("myBtn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName("close1")[0];
 
 // When the user clicks the button, open the modal
 btn.onclick = function() {
